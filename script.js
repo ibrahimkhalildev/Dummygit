04f5b7722c4814756d63c9c44b34adfabf2af3f1
Ibrahim Khalil <ibrahimkholil01@gmail.com>
2024-10-03 12:33:48 +0600

    Erase

--- script.js
+++ script.js
@@ -12,6 +12,5 @@ document.getElementById("feature_3.0.1").innerHTML = functon.make_sum();
 //Another Feature for Three
 document.getElementById("feature_3.1.0").Infinity = functon.driver();
 
-//Testing Checkout Function
-document.getElementById("feature_3.1.0").Infinity = functon.testing();
+asdf
 
