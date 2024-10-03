a3ae3ae3c44070d03978dbef7e3db5cb75d376b8
Ibrahim Khalil <ibrahimkholil01@gmail.com>
2024-10-03 12:21:09 +0600

    Testing Function

--- script.js
+++ script.js
@@ -11,3 +11,7 @@ document.getElementById("feature_3.0.1").innerHTML = functon.make_sum();
 
 //Another Feature for Three
 document.getElementById("feature_3.1.0").Infinity = functon.driver();
+
+//Testing Checkout Function
+document.getElementById("feature_3.1.0").Infinity = functon.testing();
+
